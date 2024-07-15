ddc3a6e4dad496f318fada0fc0a0f227514eabe9
Monkeyman520 <2918508198@qq.com>
2024-07-15 14:54:54 +0800

    ⚡ feat: remove some plugin in dev-container

--- lua/community.lua
+++ lua/community.lua
@@ -12,16 +12,17 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
 
   -- languge
-  { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.typescript" },
+  -- { import = "astrocommunity.pack.typescript" },
 
   -- jump
   {
